5d68ebbb09d06cd5a01126129e0b708284ab1c1b
ignacioVillarreal2003 <ignaciovillarreal20031231@gmail.com>
2023-12-08 19:30:13 -0300

    update

--- my-app/src/app/components/welcome/welcome.component.ts
+++ my-app/src/app/components/welcome/welcome.component.ts
@@ -1,99 +1,86 @@
 import { Component, OnInit, ElementRef, NgZone } from '@angular/core';
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit {
 
-  private scene!: THREE.Scene;
-  private camera!: THREE.PerspectiveCamera;
-  private renderer!: THREE.WebGLRenderer;
-  private loader: GLTFLoader;
+  constructor(private ngZone: NgZone) { }
 
-  constructor(private ngZone: NgZone) {
-    this.loader = new GLTFLoader();
-  }
+  scene: THREE.Scene = new THREE.Scene();
+  camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer();
+  geometries: THREE.Mesh[] = [];
 
   ngOnInit() {
-    this.initThree();
-    this.createScene();
-    this.createCamera();
-    this.createRenderer();
-    this.loadGLBModel();
-    this.render();
+    this.modelo3D()
   }
 
-  private initThree() {
-    this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    this.renderer = new THREE.WebGLRenderer();
-  }
+  modelo3D() {
+    // Escena
+    this.scene.background = new THREE.Color(0xebd3eb);
 
-  private createScene() {
-    this.scene.background = new THREE.Color(0xffffff);
+    // Agregar luz ambiental
     const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-  this.scene.add(ambientLight);
+    this.scene.add(ambientLight);
 
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-  directionalLight.position.set(1, 1, 1).normalize();
-  this.scene.add(directionalLight);
-  }
+    // Agregar luz direccional
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(1, 1, 1).normalize();
+    this.scene.add(directionalLight);
 
-  private createCamera() {
-    this.camera.position.set(0, 0, 10);  // Cambia los valores según sea necesario
-  }
+    // Camara
+    this.camera.position.set(0, 0, 20);
+    this.camera.lookAt(0, 0, 0);
 
-  private createRenderer() {
+    // Renderizado
     this.renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(this.renderer.domElement);
-  }
+    const container = document.querySelector('.canvas') as HTMLElement;
+    container.appendChild(this.renderer.domElement);
 
-  model: any = null;
+    /* Figuras */
+    const cubeGeometry = new THREE.BoxGeometry(5, 5, 5);
+    const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    this.geometries.push(cube);
+    this.scene.add(cube);
 
-  rotar(){
-    // Supongamos que tienes una referencia al objeto que deseas rotar
-const objectToMove = this.model;
+    const cylinderGeometry = new THREE.CylinderGeometry(5, 5, 10, 32);
+    const cylinderMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    const cylinder = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
+    this.geometries.push(cylinder)
+    this.scene.add(cylinder);
 
-// Movimiento en el eje X
-objectToMove.position.x += 5;
+    const donutGeometry = new THREE.TorusGeometry(5, 1, 16, 100);
+    const donutMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+    const donut = new THREE.Mesh(donutGeometry, donutMaterial);
+    this.geometries.push(donut)
+    this.scene.add(donut);
 
-// Movimiento en el eje Y
-objectToMove.position.y -= 2;
+    const pyramidGeometry = new THREE.ConeGeometry(5, 10, 4);
+    const pyramidMaterial = new THREE.MeshBasicMaterial({ color: 0xff00ff });
+    const pyramid = new THREE.Mesh(pyramidGeometry, pyramidMaterial);
+    this.geometries.push(pyramid)
+    this.scene.add(pyramid);
 
-// Movimiento en el eje Z
-objectToMove.position.z += 1;
-
-  }
-  private loadGLBModel() {
-    this.loader.load(
-      '../../../assets/Basic Spaceship.glb',
-      (gltf) => {
-        // Cuando el modelo se carga exitosamente
-        const model = gltf.scene;
-        this.scene.add(model);
-        this.model = model;
-      },
-      undefined,
-      (error) => {
-        console.error('Error al cargar el modelo GLB', error);
-      }
-    );
-  }
-
-  private render() {
+    // Animaciones
     this.ngZone.runOutsideAngular(() => {
       const animate = () => {
         requestAnimationFrame(animate);
-        // Actualiza la escena aquí
+
+        // Animaciones de las geometrías
+        this.geometries.forEach(geometry => {
+          geometry.rotation.x += 0.01;
+        });
+
         this.renderer.render(this.scene, this.camera);
       };
-
       animate();
     });
   }
+
 
 }
